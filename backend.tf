9a24a40629176381c6d98b0fdd892030f88bc93a
Morex <dejimorenigbade@gmail.com>
2026-08-01 17:32:55 +0100

    Remove encryption and lockfile settings from S3 backend configuration

--- backend.tf
+++ backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket       = "deenterraformstate"
     key          = "swift-devops-demo/terraform.tfstate"
     region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
   }
 }
