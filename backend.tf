0b3693cd2304ef58326fd5ee5bf1f1ef091b7578
Morex <dejimorenigbade@gmail.com>
2026-08-01 17:41:23 +0100

    Format backend configuration for improved readability

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "deenterraformstate"
-    key          = "swift-devops-demo/terraform.tfstate"
-    region       = "us-east-1"
+    bucket = "deenterraformstate"
+    key    = "swift-devops-demo/terraform.tfstate"
+    region = "us-east-1"
   }
 }
